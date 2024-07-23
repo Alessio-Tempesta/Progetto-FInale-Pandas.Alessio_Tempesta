d79cc810f93567de2697807ec8d35997ab973d40
Alessio-Tempesta <aletem999@gmail.com>
2024-07-23 16:51:32 +0200

    fatti esercizi

--- analisiPredittiva.py
+++ analisiPredittiva.py
@@ -3,7 +3,17 @@
 # Valutazione del modello: 20-30 minuti per calcolare le metriche di valutazione come l'accuratezza e l'AUC.
 
 
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
 # Creo classe modello
 class ModelloPredittivo:
     def __init__(self, dati):
         self.dati = dati
+        self.modello = None
+        
